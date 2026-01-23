acce5ca4ebbfa7d783538b1d1576c7116fe3a221
Wooyoung Jang <wooyoung.jang@uta.edu>
2026-01-23 15:12:46 -0600

    job submission scripts updated

--- dipole/scripts/agent.sh
+++ dipole/scripts/agent.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # parse arguments from jobsub
-MAG_FIELD=$1
-RUN_NUM=$2
-NSUBRUNS=$3
+EXE_FILE=$1
+MACRO_FILE=$2
+MAG_FIELD=$3
+RUN_NUM=$4
+NSUBRUNS=$5
+BASE_DATA_DIR=$6
 
-BASE_DATA_DIR="/pnfs/dune/scratch/users/wyjang/dune_mixedpol/dipole"
 SEED=$((RUN_NUM * 1000 + PROCESS))
 OUTPUT_FILE="result_${RUN_NUM}_${SEED}_${MAG_FIELD}.root"
 
@@ -14,14 +16,15 @@ source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
 spack load expat@2.5.0
 spack load geant4@10.6.1
 spack load ifdhc@2.8.0
+ifdh mkdir_p $BASE_DATA_DIR/dune/mirage/dipole/run${RUN_NUM}
 export G4DIR=$(spack location -i geant4@10.6.1)
 export Geant4_DIR=$G4DIR/lib64/Geant4-10.6.1
 export CMAKE_PREFIX_PATH=$G4DIR:$(spack location -i expat@2.5.0):$CMAKE_PREFIX_PATH
 source $G4DIR/bin/geant4.sh
 
 cd ${CONDOR_DIR_INPUT}
-chmod +x ./dipole_defocus
-./dipole_defocus ./run1.mac $MAG_FIELD $SEED $HOME/$OUTPUT_FILE
+chmod +x ./$EXE_FILE
+./$EXE_FILE ./$MACRO_FILE $MAG_FIELD $SEED $HOME/$OUTPUT_FILE
 
 if [ -f "$HOME/$OUTPUT_FILE" ]; then
     echo "Transferring $OUTPUT_FILE to $BASE_DATA_DIR..."
--- dipole/scripts/submit_grid.sh
+++ dipole/scripts/submit_grid.sh
@@ -5,20 +5,20 @@ RUN_NUM=2
 NSUBRUNS=100
 
 # Submit a job to the grid
-
 BASE_APP_DIR="/exp/dune/app/users/wyjang"
-EXE_FILE="$BASE_APP_DIR/bin/dipole_defocus"
+BASE_DATA_DIR="/pnfs/dune/scratch/users/wyjang/dune/mirage/dipole"
+EXE_FILE="$BASE_APP_DIR/bin/mirage"
 MACRO_FILE="$BASE_APP_DIR/share/dune/mirage/dipole/macros/POT_100k.mac"
-OUTPUT_DATA_DIR="$BASE_DATA_DIR/dune/mirage/dipole/run${RUN_NUM}"
+OUTPUT_DATA_DIR="$BASE_DATA_DIR/run${RUN_NUM}"
 
-ifdh mkdir_p $OUTPUT_DATA_DIR
+ifdh ls $OUTPUT_DATA_DIR >/dev/null 2>&1 || ifdh mkdir_p $OUTPUT_DATA_DIR
 OUTPUT_FILE="result_${RUN_NUM}_${SEED}_${MAG_FIELD}.root"
 
 echo "Preparing job assets ... "
 ASSET_FILE_NAME="scripts_$(date +%Y%m%d_%H%M).tar.gz"
 echo "Target: ${ASSET_FILE_NAME}"
 echo "📦 Packing your scripts into a suitcase ($ASSET_FILE_NAME)..."
-tar -cvzf $ASSET_FILE_NAME -C $EXE_FILE -C $MACRO_FILE
+tar -cvzf $ASSET_FILE_NAME -C $BASE_APP_DIR/bin mirage -C $BASE_APP_DIR/share/dune/mirage/dipole/macros POT_100k.mac
 
 if [ $? -ne 0 ]; then
     echo "Error: Failed to create tarball."
@@ -26,6 +26,8 @@ if [ $? -ne 0 ]; then
 fi
 
 # 3. Compression result
+ORIG_SIZE_BYTES=$(stat -c%s "$EXE_FILE" "$MACRO_FILE" | awk '{s+=$1} END {print s}')
+ORIG_SIZE=$((ORIG_SIZE_BYTES / 1024))
 COMP_SIZE=$(du -k "$ASSET_FILE_NAME" | awk '{print $1}')
 
 # 4. 결과 출력 및 비교
@@ -43,6 +45,7 @@ jobsub_submit -N $NSUBRUNS \
     --expected-lifetime=8h \
     --group=dune \
     --resource-provides=usage_model=OPPORTUNISTIC,DEDICATED \
-    --tar_file_name dropbox://$(pwd)/$ASSET_FILE_NAME
+    -f dropbox://$EXE_FILE \
+    -f dropbox://$MACRO_FILE \
     file:///exp/dune/app/users/wyjang/share/dune/mirage/dipole/scripts/agent.sh \
-    $MAG_FIELD $RUN_NUM $NSUBRUNS
+    mirage POT_100k.mac $MAG_FIELD $RUN_NUM $NSUBRUNS $BASE_DATA_DIR
